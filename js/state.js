8efbdf85dd9d1e82a0ec08312a882fcac4425e3f
hermesonjeremias <hermesonjeremias@gmail.com>
2026-07-23 20:57:18 -0300

    Criar state.js

---
+++ js/state.js
@@ -0,0 +1,11 @@
+/* ==========================================================================
+   STATE.JS - ESTADO GLOBAL E ESTRUTURA DE DADOS
+   ========================================================================== */
+
+const gameState = {
+    gold: 100,
+    prestige: 0,
+    maxMembers: 4,
+    adventurers: [],
+    activeQuests: []
+};
